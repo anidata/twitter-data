bda298bd17c1e0f0e9855d99da92c44314d2da5f
sethtomy <sethtomy@gmail.com>
2018-09-14 11:41:05 -0400

    Update config.py

--- search_api/config.py
+++ search_api/config.py
@@ -1,4 +1,4 @@
-twitter_anidata_consumer_key = "mCllXqP17zEIZUx4pv2uLwo8b"
-twitter_anidata_consumer_secret = "uk6nLkwpDmInvrzTazNLtr7JIAIz6tOCsSEOTQRD6bpc46vTPM"
-twitter_anidata_access_token = "347358365-U4i2mdcdrUtU6S28pgNUmW3fJgogQKDBBYOcIkY8"
-twitter_anidata_access_token_secret = "	yAFgtqGFyUfpJZpZrgRGd9BUA3QOJFcR7xlcFujpHtNGS"
+twitter_anidata_consumer_key = ""
+twitter_anidata_consumer_secret = ""
+twitter_anidata_access_token = ""
+twitter_anidata_access_token_secret = "	"
