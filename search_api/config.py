b0cccaa2bad19c784f1d0b95b41b8608e4114755
sethtomy <sethtomy@gmail.com>
2018-09-14 11:41:16 -0400

    Update config.py

--- search_api/config.py
+++ search_api/config.py
@@ -1,4 +1,4 @@
 twitter_anidata_consumer_key = ""
 twitter_anidata_consumer_secret = ""
 twitter_anidata_access_token = ""
-twitter_anidata_access_token_secret = "	"
+twitter_anidata_access_token_secret = ""
